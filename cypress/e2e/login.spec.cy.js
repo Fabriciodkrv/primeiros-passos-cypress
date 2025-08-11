2c16f62de3543d6b32e31e4c2a1b2bd862e37084
Fabriciodkrv <fabriciopeq.kr@gmail.com>
2025-08-11 16:27:26 -0300

    User update scenario added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -7,17 +7,37 @@ describe('Orange HRM Test', () => {
    loginButton: "[type='submit']",
    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
    dashboardGrid: '.orangehrm-dashboard-grid',
-   wrongCredentialAlert: '.oxd-alert'
+   wrongCredentialAlert: '.oxd-alert',
+   myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+   firstNameFiled: '[name="firstName"]',
+   lastNameField: '[name="lastName"]',
+   genericField: '.oxd-input--active',
+   dataField: '[placeholder="yyyy-dd-mm"]',
+   dataCloseButton: ".--close",
+   submitButton: '[type="submit"]'
+   
  } 
              
-  it('Login - Success', () => {
+  it.only('User info Update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorslist.usernameField).type(userData.userSeccess.username)
     cy.get(selectorslist.passwordField).type(userData.userSeccess.password)
     cy.get(selectorslist.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorslist.dashboardGrid)
- 
+    cy.get(selectorslist.myInfoButton).click()
+    cy.get(selectorslist.firstNameFiled).clear().type('firstNameTeste')
+    cy.get(selectorslist.lastNameField).clear().type('lastNameteste')
+    cy.get(selectorslist.genericField).eq(3).clear().type('9876')
+    cy.get(selectorslist.genericField).eq(4).clear().type('1597530')
+    cy.get(selectorslist.genericField).eq(5).clear().type('fabricio56897')
+    cy.get(selectorslist.dataField).eq(0).clear().type('2025-10-05')
+    cy.get(selectorslist.dataCloseButton).click()
+    cy.get(selectorslist.dataField).eq(1).clear().type('1981-10-15')
+    cy.get(selectorslist.dataCloseButton).click() 
+    cy.get(selectorslist.submitButton).eq(0).click()
+    cy.get('body').should('contain' ,'Successfully Updated')
+    cy.get('.oxd-toast-close')
   })
 
   it('Login - Fail', () => {
